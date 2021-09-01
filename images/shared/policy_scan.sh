1eae277e05468f797ee5c901cb4fc081fba57a88
Rylan Hazelton <rylan_hazelton@intuit.com>
2021-09-01 13:23:03 -0700

    update script

--- images/shared/policy_scan.sh
+++ images/shared/policy_scan.sh
@@ -2,11 +2,10 @@
 set -e
 
 echo "Scanning code via Policy Guard..."
-# TODO asset ID
-cdk synth | curl -sS -o pg_scan_result.json -F data=@- "https://policyguard-api.policyengine.v2.dev.a.intuit.com/v1/policyguard/scan-code/file?asset_id=1234567890123456789"
+# TODO asset ID & repo information
+cdk synth | curl -sS -o pg_scan_result.json -F data=@- "https://policyguard-api.policyengine.v2.dev.a.intuit.com/v1/policyguard/scan-code/file"
 HUMAN_READABLE=`jq -r .humanReadable pg_scan_result.json`
 if [ -z "${HUMAN_READABLE}" ] || [ "${HUMAN_READABLE}" == "null" ]; then
-    # there was an error, echo it and fail
     echo "Error contacting Policy Guard, skipping scan"
     cat pg_scan_result.json
     echo
