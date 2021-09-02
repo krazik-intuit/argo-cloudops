b56e2843094edc674b5cf559ce732306fa60d168
Rylan Hazelton <rylan_hazelton@intuit.com>
2021-09-01 17:35:46 -0700

    update script

--- images/shared/policy_scan.sh
+++ images/shared/policy_scan.sh
@@ -3,19 +3,23 @@ set -e
 
 echo "Scanning code via Policy Guard..."
 # TODO asset ID & repo information
-cdk synth | curl -sS -o pg_scan_result.json -F data=@- "https://policyguard-api.policyengine.v2.dev.a.intuit.com/v1/policyguard/scan-code/file"
+cdk synth | curl -sS -o pg_scan_result.json -F data=@- "https://policyguard-api.policyengine.v2.e2e.a.intuit.com/v1/policyguard/scan-code/file"
 HUMAN_READABLE=`jq -r .humanReadable pg_scan_result.json`
+
 if [ -z "${HUMAN_READABLE}" ] || [ "${HUMAN_READABLE}" == "null" ]; then
     echo "Error contacting Policy Guard, skipping scan"
     cat pg_scan_result.json
     echo
+    rm -f pg_scan_result.json
     exit 0
 fi
 
 echo "${HUMAN_READABLE}"
 echo
 
-if [ `jq ".violations | length" pg_scan_result.json` -gt 0 ]; then
-    echo "Policy violations found, aborting"
+VIOLATIONS_COUNT=`jq ".violations | length" pg_scan_result.json`
+rm -f pg_scan_result.json
+if [ "${VIOLATIONS_COUNT}" -gt 0 ]; then
+    echo "${VIOLATIONS_COUNT} policy violations found, aborting"
     exit 1
 fi
